e13762783bb34894de5ac8e4f8e88d387ef656d3
Max Houghton <max.houghton@weareevery.com>
2021-08-06 18:35:39 +0100

    Initial commit

---
+++ RangeTracker/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using RangeTracker.ViewModels;
+using RangeTracker.Views;
+using Xamarin.Forms;
+
+namespace RangeTracker
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
